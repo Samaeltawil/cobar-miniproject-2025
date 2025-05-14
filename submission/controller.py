922a6d33f4d4bd5dcb49fad88f843ba66d1268ec
Samaeltawil <151514066+Samaeltawil@users.noreply.github.com>
2025-05-14 16:13:31 +0200

    Initial Olfactory Controller
    Navigate to the odor source

--- submission/controller.py
+++ submission/controller.py
@@ -2,6 +2,20 @@ import numpy as np
 from cobar_miniproject.base_controller import Action, BaseController, Observation
 from .utils import get_cpg, step_cpg
 
+def compute_action_from_odor(obs, timestep):
+    odor = obs["odor_intensity"][0]  # Attractive odours
+    #normalize the odor intensity
+    odor = (odor - np.min(odor)) / (np.max(odor) - np.min(odor)) # vector between 0 and 1
+    left = odor[0] + odor[2]  # Left palp + left antenna
+    right = odor[1] + odor[3]  # Right palp + right antenna
+    total = left + right + 1e-8  # Avoid division by zero
+    left_ratio = left / total
+    right_ratio = right / total
+    base_drive = 1.0
+    turn_bias = (right_ratio - left_ratio)
+    left_drive = np.clip(base_drive - turn_bias, 0.0, 1.0)
+    right_drive = np.clip(base_drive + turn_bias, 0.0, 1.0)
+    return np.array([right_drive, left_drive])
 
 class Controller(BaseController):
     def __init__(
@@ -15,12 +29,23 @@ class Controller(BaseController):
         self.quit = False
         self.cpg_network = get_cpg(timestep=timestep, seed=seed)
         self.preprogrammed_steps = PreprogrammedSteps()
+        
+
+
+   
+
+
 
     def get_actions(self, obs: Observation) -> Action:
+        
+        action = compute_action_from_odor(obs, self.cpg_network.timestep)
+
+        #print("action", action)
+
         joint_angles, adhesion = step_cpg(
             cpg_network=self.cpg_network,
             preprogrammed_steps=self.preprogrammed_steps,
-            action=np.array([1.0, 1.0]),
+            action= action,
         )
 
         return {
